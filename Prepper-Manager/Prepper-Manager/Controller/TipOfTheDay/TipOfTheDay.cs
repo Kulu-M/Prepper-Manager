10a685a567f9c2f895c3c0c4542e40f8650a865c
Lloyd Niebel <Kulu-M@users.noreply.github.com>
2018-05-23 18:24:10 +0200

    Added one more tip

--- Prepper-Manager/Prepper-Manager/Controller/TipOfTheDay/TipOfTheDay.cs
+++ Prepper-Manager/Prepper-Manager/Controller/TipOfTheDay/TipOfTheDay.cs
@@ -17,22 +17,21 @@ namespace Prepper_Manager.Controller.TipOfTheDay
         public static ObservableCollection<string> tipsOfTheDay = new ObservableCollection<string>
         {
             "Tip of the Day: Lore preppus maximus dolor sit prepparatum amet.",
-            "Tip of the Day: Never burn down your house."
+            "Tip of the Day: Never burn down your house.",
+            "Tip of the Day: Go insde when it rains or you will get wet about 76% of times."
         };
 
         public static int counterList = 0;
 
-        
+
 
         public static void startTipOfTheDayChanger()
         {
-            
-                Thread.CurrentThread.IsBackground = true;
-                DispatcherTimer timer = new DispatcherTimer();
-                timer.Interval = TimeSpan.FromSeconds(1);
-                timer.Tick += timer_Tick;
-                timer.Start();
-           
+            Thread.CurrentThread.IsBackground = true;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
         }
 
         private static void timer_Tick(object sender, EventArgs e)
